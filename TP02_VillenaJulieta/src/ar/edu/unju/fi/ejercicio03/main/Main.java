377d539fbbe34e23146406969aa2c5617435bd31
PC <PC@JULI>
2024-04-29 22:26:48 -0300

    Ejercicio03

--- TP02_VillenaJulieta/src/ar/edu/unju/fi/ejercicio03/main/Main.java
+++ TP02_VillenaJulieta/src/ar/edu/unju/fi/ejercicio03/main/Main.java
@@ -1,10 +1,16 @@
 package ar.edu.unju.fi.ejercicio03.main;
 
+import ar.edu.unju.fi.ejercicio03.constantes.Provincia;
+
 public class Main {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+    public static void main(String[] args) {
+        Provincia[] provincias = Provincia.values();
+        for (Provincia provincia : provincias) {
+            System.out.println("Provincia: " + provincia);
+            System.out.println("Cantidad de población: " + provincia.getCantidadPoblacion());
+            System.out.println("Superficie: " + provincia.getSuperficie() + " km²");
+            System.out.println("Densidad poblacional: " + provincia.calcularDensidadPoblacional() + " habitantes/km²");
+            System.out.println();
+        }
+    }
 }
